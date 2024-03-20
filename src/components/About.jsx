3c2b4c412dd014bc32cf44dc31a707f7f40fb9f2
amanchhetri <gattiflab@gmail.com>
2024-03-21 02:03:14 +0530

    1st phase

---
+++ src/components/About.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        As an enthusiastic front-end developer, I bring extensive expertise in JavaScript, React.js, Next.js, and Redux to the table. My primary focus revolves around crafting visually stunning and responsive web applications that captivate users. By harnessing the power of tools such as Tailwind CSS, Material UI, and Bootstrap, I strive to deliver seamless user experiences that leave a lasting impression.
+        <br />
+        Moreover, I excel in integrating interactive maps using technologies like Deck GL and the Google Maps API, enriching applications with dynamic and engaging functionalities. My proficiency extends to efficiently working with REST APIs, enabling seamless data exchange and enhancing the overall user experience.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
---
+++ src/constants/index.js
@@ -0,0 +1,257 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  mongodb,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'UI/UX Design',
+    icon: ux,
+  },
+  {
+    title: 'Software Testing',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'mongodb',
+    icon: mongodb,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Software Engineer (MERN)',
+    company_name: 'TalkValley EdTech',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: 'Aug 2020 - Dec 2021',
+  },
+  {
+    title: 'Software Engineer',
+    company_name: 'Tark Technologies',
+    icon: microverse,
+    iconBg: '#333333',
+    date: 'Jan 2022 - Dec 2022',
+  },
+  {
+    title: 'Frontend Developer',
+    company_name: 'Lepton Software',
+    icon: kelhel,
+    iconBg: '#333333',
+    date: 'Feb 2023 - Present',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'Crypto Sky',
+    description: 'A website showcasing a comprehensive list of cryptocurrency coins along with their respective price graphs provides users with valuable insights and information about the cryptocurrency market.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/amanchhetri/crytpo-sky',
+    demo: 'https://crytpo-sky.vercel.app/#/',
+  },
+  {
+    id: 'project-2',
+    name: 'SoundBoard',
+    description:
+      'Sounds triggered by keystrokes using Paper.js and ReactHowler.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    repo: 'https://github.com/amanchhetri/SoundBoard',
+    demo: 'https://sound-board-eight.vercel.app/',
+  },
+  {
+    id: 'project-3',
+    name: 'PictureYard',
+    description: 'A single-page application (SPA) built with React, featuring a collection of images, offers a dynamic and responsive user experience.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    repo: 'https://github.com/amanchhetri/PictureYard',
+    demo: 'https://picture-yard.vercel.app/',
+  },
+  {
+    id: 'project-4',
+    name: 'Todo List',
+    description: `A todo list application built with React.js provides a user-friendly interface for managing tasks and staying organized.`,
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: 'https://github.com/amanchhetri/todo-list',
+    demo: 'https://todo-app-iota-blond-86.vercel.app/',
+  },
+  {
+    id: 'project-5',
+    name: 'Lepton Maps',
+    description:
+      'Our SaaS company specializes in providing a range of GeoIntelligence services',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: nyeusi,
+    repo: 'https://github.com/amanchhetri/',
+    demo: 'https://leptonmaps.com/',
+  },
+];
+
+export { services, technologies, experiences, projects };
